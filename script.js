5b75004b67e90993c4b712c873c7e9b6150b5ed9
Miguel Neto <miguelrsn@gmail.com>
2018-02-14 21:30:00 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-var userName;
-const ajax = new XMLHttpRequest();
-const perfil = new XMLHttpRequest();
-let perfil_obj;
-
 function consultar() {
-    userName = document.getElementById("iBuscar").value;
-    ajax.open('GET', 'https://api.github.com/search/users?q=' + userName);
-    ajax.send();
+    const userName = document.getElementById("iBuscar").value;
 
-    ajax.addEventListener('readystatechange', function() {
-        if (ajax.readyState === 4) {
-            let resp = JSON.parse(ajax.responseText);
-            if (resp.total_count <= 0) {
-                console.log("Usuario nao existe");
-                return
-            }
-            perfil.open('GET', resp.items[0].url);
-            perfil.send();
-        }
+    const request = new XMLHttpRequest();
+    
+    request.open('GET', `https://api.github.com/users/${userName}`);
+    request.send();
 
-    })
-
-    perfil.addEventListener('readystatechange', function() {
-        if (perfil.readyState == 4) {
-            perfil_obj = JSON.parse(perfil.responseText);
-            console.log(perfil_obj);
-            document.getElementById('name').innerHTML = perfil_obj.name;
-            document.getElementById('location').innerHTML = perfil_obj.location;
-            document.getElementById('criado_em').innerHTML = perfil_obj.created_at;
-            document.getElementById('public_repos').innerHTML = perfil_obj.public_repos;
-            document.getElementById('public_gists').innerHTML = perfil_obj.public_gists;
-            document.getElementById('followers').innerHTML = perfil_obj.followers;
-            document.getElementById('following').innerHTML = perfil_obj.following;
-        }
-    })
-
-
+    request.onload = function() {
+        const perfil_obj = JSON.parse(request.responseText);
+        console.log(perfil_obj);
+        document.getElementById('name').innerHTML = perfil_obj.name;
+        document.getElementById('location').innerHTML = perfil_obj.location;
+        document.getElementById('criado_em').innerHTML = perfil_obj.created_at;
+        document.getElementById('public_repos').innerHTML = perfil_obj.public_repos;
+        document.getElementById('public_gists').innerHTML = perfil_obj.public_gists;
+        document.getElementById('followers').innerHTML = perfil_obj.followers;
+        document.getElementById('following').innerHTML = perfil_obj.following;
+    }
 }
